def2aea2db1a95cd4d8aee137f3deaaea19ab212
fullofrum <84639986+fullofrum@users.noreply.github.com>
2023-03-12 17:01:08 +0000

    adds task4 retrieval and cleaning scripts

--- Task4/raw_data/data_retrieval_script.R
+++ Task4/raw_data/data_retrieval_script.R
@@ -5,4 +5,4 @@ boing_boing_candy_2015 <- read_xlsx(here::here("raw_data/boing-boing-candy-2015.
 boing_boing_candy_2016 <- read_xlsx(here::here("raw_data/boing-boing-candy-2016.xlsx"))
 boing_boing_candy_2017 <- read_xlsx(here::here("raw_data/boing-boing-candy-2017.xlsx"))
 
-
+glimpse(boing_boing_candy_2015)
