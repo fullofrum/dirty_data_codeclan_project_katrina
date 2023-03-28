48d71b991f82fbf460440e306f96b875a861ebdd
fullofrum <84639986+fullofrum@users.noreply.github.com>
2023-03-28 12:04:38 +0100

    adds modified task2 cleaning script and analysis

--- Task2/data_cleaning_scripts/task2_cleaning_script.R
+++ Task2/data_cleaning_scripts/task2_cleaning_script.R
@@ -3,7 +3,29 @@ library(janitor)
 library(lubridate)
 library(readxl)
 library(here)
+library(stringr)
 
 
 cakes <- read.csv(here::here("raw_data/cake-ingredients-1961.csv"))
 cake_codes <- read_csv(here::here("raw_data/cake_ingredient_code.csv"))
+
+cakes_pivot <- cakes %>% 
+pivot_longer(cols = AE:ZH,
+             names_to = "code",
+             values_to = "measurement", as.numeric("measurement"))
+
+cakes_clean <- cakes_pivot %>% 
+  left_join(cake_codes, by = "code") %>%    # joins the two datasets
+  na.omit() %>%                             # removes rows which contian NA
+  mutate(cake = str_trim(Cake), .before = code) %>%   # removes whitespace in Cake column
+  subset(select = - Cake )                  #removes old cake column
+  
+
+
+write.csv(cakes_clean, "clean_data/cakes.csv")
+  
+
+  
+
+
+ 
